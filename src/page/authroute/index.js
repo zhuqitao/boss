c18da383f67d57f0aaee8adc361b2d7d7fbd4d6f
zhuqitao <1362335135@qq.com>
2018-12-09 17:53:58 +0800

    'proxy转移由package.json转移到src/setupProxy.js'

---
+++ src/page/authroute/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class AutnRoute extends React.Component{
+    componentDidMount() {
+        // 是否登录
+        // /login和/register不需要跳转
+        // 用户的type 是boss还是牛人
+    }
+    
+    render() {
+        return (<div>auth</div>)
+    }
+}
--- src/page/login/index.js
+++ src/page/login/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import styles from "./index.module.css";
 import {Button} from 'antd-mobile';
+import axios from 'axios';
 class Login extends React.Component {
 
+    componentDidMount() {
+        axios.get('/api/user/info');
+    }
     render() {
         return (
             <div className={styles.login}>
